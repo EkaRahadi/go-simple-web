d8abcbb634a859d2ac0d859bdeb4ba30309d52c6
Eka Rahadi <ekarahadi98@gmail.com>
2022-10-25 20:34:17 +0700

    learn how to set status code also how to set and get cookie

--- main.go
+++ main.go
@@ -9,9 +9,61 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, world!")
 }
 
+func QueryHandler(w http.ResponseWriter, r *http.Request) {
+	firstName := r.URL.Query().Get("first_name")
+	lastName := r.URL.Query().Get("last_name")
+	fmt.Fprintf(w, "Hello, %s %s", firstName, lastName)
+}
+
+func QueryHandlerArray(w http.ResponseWriter, r *http.Request) {
+	queries := r.URL.Query()
+	var name []string = queries["name"]
+	w.Header().Add("x-powered-by", "eka-rahadi")
+	connection := r.Header.Get("connection")
+	fmt.Println(connection)
+	fmt.Fprintf(w, "Hello, %s %s", name[0], name[1])
+}
+
+func FormPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	firstName := r.PostForm.Get("first_name")
+	lastName := r.PostForm.Get("last_name")
+
+	fmt.Fprintf(w, "Hello, %s %s", firstName, lastName)
+}
+
+func CookieAndStatusCodeHandler(w http.ResponseWriter, r *http.Request) {
+	firstName := r.URL.Query().Get("first_name")
+	if firstName == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "No First Name!")
+	} else {
+		cookie := new(http.Cookie)
+		cookie.Name = "first_name"
+		cookie.Value = firstName
+		http.SetCookie(w, cookie)
+		fmt.Fprintf(w, "Hello, %s", firstName)
+	}
+}
+
+func GetCookieAndStatusCodeHandler(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("first_name")
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "No Cookie!")
+	} else {
+		fmt.Fprintf(w, "Hello, %s", cookie.Value)
+	}
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", IndexHandler)
+	mux.HandleFunc("/say-hello", QueryHandler)
+	mux.HandleFunc("/say-hello-array", QueryHandlerArray)
+	mux.HandleFunc("/say-hello-post", FormPostHandler)
+	mux.HandleFunc("/say-hello-post-cookie", CookieAndStatusCodeHandler)
+	mux.HandleFunc("/say-hello-get-cookie", GetCookieAndStatusCodeHandler)
 
 	err := http.ListenAndServe(":9000", mux)
 	if err != nil {
--- main_test.go
+++ main_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -33,6 +35,109 @@ func TestMain(t *testing.T) {
 			body, _ := io.ReadAll(response.Body)
 			bodyStr := string(body)
 			assert.Equal(t, test.expected, bodyStr, "Should match 'Hello, world!'")
+		})
+	}
+}
+
+func TestMainQuery(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  []string
+		expected string
+	}{
+		{
+			name:     "Hello, Eka Rahadi",
+			request:  []string{"Eka", "Rahadi"},
+			expected: "Hello, Eka Rahadi",
+		},
+		{
+			name:     "Hello, Elba Ayu",
+			request:  []string{"Elba", "Ayu"},
+			expected: "Hello, Elba Ayu",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet,
+				fmt.Sprintf("http://localhost:9000/say-hello?first_name=%s&last_name=%s", test.request[0], test.request[1]), nil)
+			recorder := httptest.NewRecorder()
+
+			QueryHandler(recorder, request)
+
+			response := recorder.Result()
+			body, _ := io.ReadAll(response.Body)
+			bodyStr := string(body)
+			assert.Equal(t, test.expected, bodyStr, fmt.Sprintf("Should Match 'Hello, %s %s'", test.request[0], test.request[1]))
+		})
+	}
+}
+
+func TestMainQueryArray(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  []string
+		expected string
+	}{
+		{
+			name:     "Hello, Eka Rahadi",
+			request:  []string{"Eka", "Rahadi"},
+			expected: "Hello, Eka Rahadi",
+		},
+		{
+			name:     "Hello, Elba Ayu",
+			request:  []string{"Elba", "Ayu"},
+			expected: "Hello, Elba Ayu",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet,
+				fmt.Sprintf("http://localhost:9000/say-hello-array?name=%s&name=%s", test.request[0], test.request[1]), nil)
+			recorder := httptest.NewRecorder()
+
+			QueryHandlerArray(recorder, request)
+
+			response := recorder.Result()
+			body, _ := io.ReadAll(response.Body)
+			bodyStr := string(body)
+			assert.Equal(t, test.expected, bodyStr, fmt.Sprintf("Should Match 'Hello, %s %s'", test.request[0], test.request[1]))
+		})
+	}
+}
+
+func TestFormPostHandler(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  []string
+		expected string
+	}{
+		{
+			name:     "Hello, Eka Rahadi",
+			request:  []string{"Eka", "Rahadi"},
+			expected: "Hello, Eka Rahadi",
+		},
+		{
+			name:     "Hello, Elba Ayu",
+			request:  []string{"Elba", "Ayu"},
+			expected: "Hello, Elba Ayu",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			requestBody := strings.NewReader(fmt.Sprintf("first_name=%s&last_name=%s", test.request[0], test.request[1]))
+			request := httptest.NewRequest(http.MethodPost, "http://localhost:9000/say-hello-post", requestBody)
+			request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+			recorder := httptest.NewRecorder()
+
+			FormPostHandler(recorder, request)
+
+			response := recorder.Result()
+			body, _ := io.ReadAll(response.Body)
+			bodyStr := string(body)
+			assert.Equal(t, test.expected, bodyStr, fmt.Sprintf("Should Match 'Hello, %s %s'", test.request[0], test.request[1]))
 		})
 	}
 }
